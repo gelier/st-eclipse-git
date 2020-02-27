36d224d925bedc8e15899e392bf0617748a91ed6
gelier <guiller.dalit@sjsu.edu>
2020-02-26 23:15:33 -0800

    Shape

--- implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
+++ implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
@@ -1,5 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 public class TetrisBlock {
+
+	private final int WIDTH = 4;
+	private final int HEIGHT = 2;
 	
+	private ArrayList[][]shape;
+	/**
+	 * 
+	 * 
+	 */
+	public TetrisBlock() {
+		/* example of shapes
+		 * [][][][]
+		 * 			   []
+		 * [][][][]  [][][]
+		 * 		 []
+		 * 
+		 * []
+		 * [][][][]
+		 * 
+		 * The width is always <= 4 and height is always <= 2
+		 * */
+		shape = new ArrayList[WIDTH][HEIGHT];
+	}
+	protected void generateShape(){
+		
+	
+	}
+	protected ArrayList<Block> getShape(){
+		
+		return null;
+	}
 }
