46d49501c7b3236dbe328145da3551deda95fcc1
iamkennethhuang <kenneth.j.huang@sjsu.edu>
2020-04-21 17:47:47 -0700

    defined all tetris block methods

--- implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
+++ implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
@@ -23,11 +23,63 @@ public class TetrisBlock {
 		 * The width is always <= 4 and height is always <= 2
 		 * */
 		shape = new ArrayList[WIDTH][HEIGHT];
+		this.shape = shape;
 	}
-	protected void generateShape(){
-		
 	
+	public TetrisBock(ArrayList[][] shape) {
+		if(shape.length == HEIGHT) {
+			if(shape[0].length == WIDTH) {
+				this.shape = shape;
+			}
+		}
+		else {
+			throw new IOException("Shape Array doesn't fit the requirement"); 
+		}
 	}
+	
+	protected ArrayList[][] generateShape(){
+		
+	}
+	
+	protected void generateShapeOnArray() {
+		
+	}
+	
+	protected ArrayList[][] generateSquare() {
+		ArrayList shape[][] = {{1,1,0,0},{1,1,0,0}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateStraight() {
+		ArrayList shape[][] = {{1,1,1,1},{0,0,0,0}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateT() {
+		ArrayList shape[][] = {{1,1,1,0},{0,1,0,0}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateL() {
+		ArrayList shape[][] = {{1,0,0,0},{1,1,1,1}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateSkew() {
+		ArrayList shape[][] = {{0,1,1,0},{1,1,0,0}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateFlipL() {
+		ArrayList shape[][] = {{0,0,0,1},{1,1,1,1}};
+		return shape;
+	}
+	
+	protected ArrayList[][] generateFlipZ() {
+		ArrayList shape[][] = {{1,1,0,0},{0,1,1,0}};
+		return shape;
+	}
+	
 	protected ArrayList[][] getShape(){
 		
 		return null;
