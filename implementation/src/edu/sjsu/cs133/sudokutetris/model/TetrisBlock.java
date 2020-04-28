734d9c229a29497c51c6e16ed684fbd60bf30dd5
Shruti Panchal <shruti.panchal@sjsu.edu>
2020-04-28 11:16:19 -0700

    Commented the Tetris shapes generation

--- implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
+++ implementation/src/edu/sjsu/cs133/sudokutetris/model/TetrisBlock.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.awt.Color;
+import java.awt.Point;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -7,6 +9,76 @@ public class TetrisBlock {
 	private final int WIDTH = 4;
 	private final int HEIGHT = 2;
 	private ArrayList<int[][]> shapes;
+	
+	
+	//Different shapes of Tetris.
+	
+	
+	private final Point[][][] Tetraminos = {
+			// I-Piece
+			{
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) },
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) }
+			},
+			
+			// J-Piece
+			{
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 0) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 2) },
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(0, 2) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 0) }
+			},
+			
+			// L-Piece
+			{
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 2) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2) },
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(0, 0) },
+				{ new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 0) }
+			},
+			
+			// O-Piece
+			{
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) }
+			},
+			
+			// S-Piece
+			{
+				{ new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1) },
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) },
+				{ new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1) },
+				{ new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) }
+			},
+			
+			// T-Piece
+			{
+				{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1) },
+				{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) },
+				{ new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(1, 2) },
+				{ new Point(1, 0), new Point(1, 1), new Point(2, 1), new Point(1, 2) }
+			},
+			
+			// Z-Piece
+			{
+				{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
+				{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) },
+				{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
+				{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) }
+			}
+	};
+	
+	private final Color[] tetraminoColors = {
+		Color.cyan, Color.blue, Color.orange, Color.yellow, Color.green, Color.pink, Color.red
+	};
+	
+	
+	
+
 
 	/**
 	 * 
